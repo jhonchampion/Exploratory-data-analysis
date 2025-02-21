58935b6bb822a08648996df2179f6a389e5438c7
John umezinwa <umezinwa19@gmail.com>
2025-02-21 03:57:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ with col1:
     st.plotly_chart(fig)
 with col2:
     school = ivsa['University '].value_counts()
-    fig = px.bar(school, color=school, title='Delegates by School;', color_continuous_scale='Viridis', labels={'value': 'Count',})
+    fig = px.bar(school, title='Delegates by School;', color_continuous_scale='Viridis', labels={'value': 'Count',})
     st.plotly_chart(fig)
     
 
